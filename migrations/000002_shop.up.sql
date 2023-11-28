4ea4dc3681753ba0cc9a50ecdf2a98b65fef35b9
Samandar <samandar0311@gmail.com>
2023-11-28 11:36:13 +0500

    fix: changed user_id to owner_id in the shop

--- migrations/000002_shop.up.sql
+++ migrations/000002_shop.up.sql
@@ -1,10 +1,10 @@
 CREATE TABLE "shop" (
   "id" SERIAL PRIMARY KEY NOT NULL,
   "name" varchar(255) UNIQUE NOT NULL,
-  "user_id" int NOT NULL,
+  "owner_id" int NOT NULL,
   "created_at" timestamp NOT NULL DEFAULT (current_timestamp),
   "updated_at" timestamp NOT NULL DEFAULT (current_timestamp),
   "deleted_at" timestamp
 );
 
-ALTER TABLE "shop" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+ALTER TABLE "shop" ADD FOREIGN KEY ("owner_id") REFERENCES "users" ("id");
