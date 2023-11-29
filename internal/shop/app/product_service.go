de36c79d6b8f21cf0cc6da8d430f0534aed844a8
xbakhromjon <xbakhromjon@gmail.com>
2023-11-29 14:25:14 +0500

    shop: get one product business logic and unit test completed

--- internal/shop/app/product_service.go
+++ internal/shop/app/product_service.go
@@ -1,9 +1,41 @@
 package app
 
-import "golang-project-template/internal/shop/domain"
+import (
+	"database/sql"
+	"golang-project-template/internal/shop/adapter"
+	"golang-project-template/internal/shop/domain"
+)
 
 type ProductService interface {
 	Add(req domain.NewProduct) (int, error)
 	GetOne(id int) (*domain.Product, error)
 	GetAllByShopId(shopId int) ([]*domain.Product, error)
+}
+
+func NewProductService(db *sql.DB) ProductService {
+
+	return &productService{repository: adapter.NewProductAdapter(db)}
+}
+
+type productService struct {
+	repository domain.ProductRepository
+}
+
+func (p *productService) Add(req domain.NewProduct) (int, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (p *productService) GetOne(id int) (*domain.Product, error) {
+	product, err := p.repository.FindById(id)
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (p *productService) GetAllByShopId(shopId int) ([]*domain.Product, error) {
+	//TODO implement me
+	panic("implement me")
 }
---
+++ internal/shop/app/product_service_test.go
@@ -0,0 +1,44 @@
+package app
+
+import (
+	"errors"
+	"fmt"
+	"golang-project-template/internal/shop/adapter"
+	"golang-project-template/internal/shop/adapter/mock"
+	"golang-project-template/internal/shop/domain"
+	"reflect"
+	"testing"
+)
+
+func TestGetOneProduct(t *testing.T) {
+	underTest := productService{repository: mock.NewMockProductAdapter(nil)}
+	t.Run("with correct id", func(t *testing.T) {
+		id := 1
+		got, _ := underTest.GetOne(id)
+		want := domain.Product{
+			Id:     id,
+			Name:   "T-shirt",
+			Price:  100,
+			ShopId: 1,
+		}
+		if !reflect.DeepEqual(want, *got) {
+			t.Errorf("want %+v but got %+v", want, got)
+		}
+	})
+
+	t.Run("with invalid id", func(t *testing.T) {
+		id := 2
+		_, err := underTest.GetOne(id)
+		want := adapter.ErrProductNotFound{Err: fmt.Sprintf("Product not found with %d", id)}
+
+		if err == nil {
+			t.Errorf("expected %T, got nil", want)
+			return
+		}
+
+		var got *adapter.ErrProductNotFound
+		if !errors.As(err, &got) {
+			t.Errorf("want %T, got %T", want, err)
+		}
+	})
+}
