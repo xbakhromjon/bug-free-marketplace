f2548704c73dc1d0c037d5d1d2db5500263ba227
moxirboy <mox777boy@gmail.com>
2023-12-12 21:09:13 +0500

    added migration

---
+++ migrations/000008_cart_items.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "cart_items" (
+                           "id" SERIAL PRIMARY KEY NOT NULL,
+                           "product_id" int NOT NULL,
+                           "cart_id" int NOT NULL,
+                           "unit_price" integer NOT NULL,
+                           "quantity" integer NOT NULL,
+);
+
+ALTER TABLE "cart_items" ADD FOREIGN KEY ("product_id") REFERENCES "product" ("id");
+ALTER TABLE "cart_items" ADD FOREIGN KEY ("cart_id") REFERENCES "cart" ("id");
