76977e7cc0fc763c650b4a6b82f88bb23629ed75
usmanov0 <azikusmanov882@gmail.com>
2023-12-21 22:36:46 +0500

    added :some errors

--- internal/basket/domain/error.go
+++ internal/basket/domain/error.go
@@ -1,13 +1,14 @@
 package domain
 
 const (
-	ErrCartItemNotFound       = Err("Cart item not found")
-	ErrCartCreationFailed     = Err("Cart creation failed")
-	ErrCartItemCreationFailed = Err("Cart Item creation failed")
-	ErrCartUpdateFailed       = Err("Failed to update cart")
-	ErrIDScanFailed           = Err("Failed to scan id")
-	ErrAddItemFailed          = Err("Failed to add Item")
-	ErrDeleteItemFailed       = Err("Failed to delete Item")
+	ErrCartItemNotFound   = Err("Cart item not found")
+	ErrInvalidCartId      = Err("Invalid cart id")
+	ErrInvalidId          = Err("Invalid id")
+	ErrItemCreationFailed = Err("Cart Item creation failed")
+	ErrCartUpdateFailed   = Err("Failed to update cart")
+	ErrIDScanFailed       = Err("Failed to scan id")
+	ErrAddItemFailed      = Err("Failed to add Item")
+	ErrDeleteItemFailed   = Err("Failed to delete Item")
 )
 
 type Err string
