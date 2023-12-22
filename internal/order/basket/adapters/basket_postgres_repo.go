41e8743edb5dcaf2963b8945dd91a2c6943c6897
usmanov0 <azikusmanov882@gmail.com>
2023-12-22 18:03:57 +0500

    fix :some errors and addItem to cart

--- internal/order/basket/adapters/basket_postgres_repo.go
+++ internal/order/basket/adapters/basket_postgres_repo.go
@@ -9,8 +9,12 @@ type basketPostgresRepo struct {
 	db *pgx.Conn
 }
 
+func NewBasketRepository(db *pgx.Conn) domain.BasketRepository {
+	return &basketPostgresRepo{db: db}
+}
+
 func (b basketPostgresRepo) CreateBasket(userId int) (id int, err error) {
-	row := b.db.QueryRow("INSERT INTO cart(user_id,purchased) VALUES($1,true) RETURNING id;", userId)
+	row := b.db.QueryRow("INSERT INTO cart(user_id,purchased) VALUES($1) RETURNING id;", userId)
 	err = row.Scan(&id)
 	if err != nil {
 		return 0, err
@@ -18,6 +22,12 @@ func (b basketPostgresRepo) CreateBasket(userId int) (id int, err error) {
 	return id, err
 }
 
-func NewBasketRepository(db *pgx.Conn) domain.BasketRepository {
-	return &basketPostgresRepo{db: db}
+func (b basketPostgresRepo) AddItem(cart *domain.BasketItems) (id int, err error) {
+	row := b.db.QueryRow("INSERT INTO cart_items(cart_id,product_id, quantity) VALUES ($1,$2,$3) RETURNING id",
+		cart.CartId, cart.ProductId, cart.Quantity)
+	err = row.Scan(&id)
+	if err != nil {
+		return 0, err
+	}
+	return id, nil
 }
--- internal/order/basket/domain/repository.go
+++ internal/order/basket/domain/repository.go
@@ -2,4 +2,5 @@ package domain
 
 type BasketRepository interface {
 	CreateBasket(userId int) (int, error)
+	AddItem(cart *BasketItems) (int, error)
 }
