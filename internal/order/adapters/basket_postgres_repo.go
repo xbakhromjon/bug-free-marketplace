4bbc4cf3a7a4c5c1a4c883f5d35696528f395453
usmanov0 <azikusmanov882@gmail.com>
2023-12-23 17:45:08 +0500

    basket repository:CRUD operation

--- internal/order/adapters/basket_postgres_repo.go
+++ internal/order/adapters/basket_postgres_repo.go
@@ -14,10 +14,51 @@ func NewCartRepository(db *pgx.Conn) domain.BasketRepository {
 }
 
 func (b *basketRepo) CreateBasket(userId int) (id int, err error) {
-	row := b.db.QueryRow("INSERT INTO basket(user_id,purchased) VALUES ($1,$2) RETURNING id;", userId)
+	row := b.db.QueryRow("INSERT INTO basket(user_id,purchased) VALUES ($1,false) RETURNING id;", userId)
 	err = row.Scan(row)
 	if err != nil {
 		return 0, err
 	}
 	return id, nil
+}
+
+func (b *basketRepo) AddItem(items *domain.BasketItems) (id int, err error) {
+	row := b.db.QueryRow("INSERT INTO basket_items(basket_id,product_id,quantity) VALUES ($1,$2,$3) RETURNING id",
+		items.BasketId, items.ProductId, items.Quantity)
+
+	err = row.Scan(&id)
+	if err != nil {
+		return 0, domain.ErrIDScanFailed
+	}
+	return id, nil
+}
+
+func (b *basketRepo) GetAll(basketId int) ([]domain.BasketItems, error) {
+	row, _ := b.db.Query("SELECT * from basket_items WHERE basket_id = $1", basketId)
+	var Items []domain.BasketItems
+	for row.Next() {
+		var bItems domain.BasketItems
+		err := row.Scan(&bItems.Id, &bItems.BasketId, &bItems.ProductId, &bItems.Quantity)
+		if err != nil {
+			return nil, domain.ErrIDScanFailed
+		}
+		Items = append(Items, bItems)
+	}
+	return Items, nil
+}
+
+func (b *basketRepo) UpdateBasketItem(basketId, quantity int) error {
+	_, err := b.db.Exec("UPDATE basket_items SET quantity = quantity + $1 WHERE basket_id = $2", quantity, basketId)
+	if err != nil {
+		return domain.ErrBasketUpdateFailed
+	}
+	return nil
+}
+
+func (b *basketRepo) UpdateBasketStatus(basketId int) error {
+	_, err := b.db.Exec("UPDATE basket SET purchased = true WHERE id = $2", basketId)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- internal/order/domain/repository.go
+++ internal/order/domain/repository.go
@@ -2,4 +2,8 @@ package domain
 
 type BasketRepository interface {
 	CreateBasket(userId int) (int, error)
+	AddItem(items *BasketItems) (int, error)
+	GetAll(basketId int) ([]BasketItems, error)
+	UpdateBasketItem(basketId, quantity int) error
+	UpdateBasketStatus(basketId int) error
 }
