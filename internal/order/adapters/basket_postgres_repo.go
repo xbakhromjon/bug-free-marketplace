dcce8be57c1e23938cc11b0399fd76e3b42fec84
usmanov0 <azikusmanov882@gmail.com>
2023-12-25 16:28:50 +0500

    query refactored

--- internal/order/adapters/basket_postgres_repo.go
+++ internal/order/adapters/basket_postgres_repo.go
@@ -55,7 +55,7 @@ func (b *basketRepo) GetBasket(basketId int) (*BasketWithItems, error) {
 }
 
 func (b *basketRepo) GetActiveBasket(userID int) (*domain.Basket, error) {
-	row := b.db.QueryRow("SELECT b.basket_id FROM basket b WHERE b.user_id = $1 AND b.purchased = false", userID)
+	row := b.db.QueryRow("SELECT b.id FROM basket b WHERE b.user_id = $1 AND b.purchased = false", userID)
 	var basket domain.Basket
 	if err := row.Scan(&basket.Id, &basket.UserId, &basket.Purchased); err != nil {
 		return nil, err
