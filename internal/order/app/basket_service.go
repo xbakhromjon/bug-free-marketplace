35625268f09de5a5e62e583163a8f5dd5698c935
usmanov0 <azikusmanov882@gmail.com>
2023-12-26 16:57:35 +0500

    basket service

--- internal/order/app/basket_service.go
+++ internal/order/app/basket_service.go
@@ -1,22 +1,23 @@
 package app
 
 import (
-	"golang-project-template/internal/order/adapters"
 	"golang-project-template/internal/order/domain"
 )
 
 type BasketService interface {
 	CreateBasket(userID int) (int, error)
 	AddItem(userId int, basket *domain.BasketItems) (int, error)
-	GetBasketById(basketId int) (*adapters.BasketWithItems, error)
+	GetBasketWithItemsById(basketId int) (*domain.BasketWithItems, error)
 	GetAll(basketId int) ([]domain.BasketItems, error)
 	UpdateBasketQuantity(bItemId, quantity int) error
 	MarkBasketAsPurchased(userId, basketId int) error
 	DeleteProduct(bItemId int) (int, error)
 }
 
-func NewCartService(repo domain.BasketRepository) BasketService {
-	return &basketService{basketRepo: repo}
+func NewBasketService(basketRepo domain.BasketRepository, repoItem domain.BasketItemRepository) BasketService {
+	return &basketService{
+		basketRepo:     basketRepo,
+		basketItemRepo: repoItem}
 }
 
 type basketService struct {
@@ -25,27 +26,38 @@ type basketService struct {
 }
 
 func (b *basketService) CreateBasket(userID int) (int, error) {
-	basketId, err := b.CreateBasket(userID)
+	basketId, err := b.basketRepo.CreateBasket(userID)
 	if err != nil {
 		return 0, domain.ErrIDScanFailed
 	}
 	return basketId, nil
 }
 
-func (b *basketService) AddItem(userId int, bItem *domain.BasketItems) (int, error) {
+func (b *basketService) AddItem(userId int, bItems *domain.BasketItems) (int, error) {
 	activeBasket, _ := b.basketRepo.GetActiveBasket(userId)
 	if !activeBasket.Purchased {
-		id, err := b.basketItemRepo.AddItem(bItem)
+		id, err := b.basketItemRepo.AddItem(bItems)
 		if err != nil {
 			return 0, domain.ErrAddItemFailed
 		}
 		return id, err
 	}
-	basketId, _ := b.basketRepo.CreateBasket(activeBasket.UserId)
-	return basketId, nil
+	newBasketId, err := b.basketRepo.CreateBasket(userId)
+	if err != nil {
+		return 0, domain.ErrBasketCreationFailed
+	}
+	id, err := b.basketItemRepo.AddItem(&domain.BasketItems{
+		BasketId:  newBasketId,
+		ProductId: bItems.ProductId,
+		Quantity:  bItems.Quantity,
+	})
+	if err != nil {
+		return 0, domain.ErrAddItemFailed
+	}
+	return id, nil
 }
 
-func (b *basketService) GetBasketById(basketId int) (*adapters.BasketWithItems, error) {
+func (b *basketService) GetBasketWithItemsById(basketId int) (*domain.BasketWithItems, error) {
 	bWithItems, err := b.basketRepo.GetBasket(basketId)
 	if err != nil {
 		return nil, err
@@ -54,7 +66,7 @@ func (b *basketService) GetBasketById(basketId int) (*adapters.BasketWithItems, 
 }
 
 func (b *basketService) GetAll(basketId int) ([]domain.BasketItems, error) {
-	cItems, err := b.GetAll(basketId)
+	cItems, err := b.basketItemRepo.GetAll(basketId)
 	if err != nil {
 		return nil, domain.ErrBasketItemsNotFound
 	}
