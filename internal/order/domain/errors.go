79e0925c10588d4e96393aef4406a0f9ff4e586a
usmanov0 <azikusmanov882@gmail.com>
2023-12-27 10:42:43 +0500

    errors :some errors

--- internal/order/domain/errors.go
+++ internal/order/domain/errors.go
@@ -1,12 +1,14 @@
 package domain
 
 const (
-	ErrBasketCreationFailed = Err("Failed to create basket")
-	ErrIDScanFailed         = Err("Failed to scan id")
-	ErrBasketUpdateFailed   = Err("Failed to update basket")
-	ErrDeleteItemFailed     = Err("Failed to delete Item")
-	ErrAddItemFailed        = Err("Couldn't update quantity")
-	ErrBasketItemsNotFound
+	ErrBasketCreationFailed  = Err("Failed to create basket")
+	ErrIDScanFailed          = Err("Failed to scan id")
+	ErrBasketUpdateFailed    = Err("Failed to update basket")
+	ErrDeleteItemFailed      = Err("Failed to delete Item")
+	ErrAddItemFailed         = Err("Couldn't update quantity")
+	ErrBasketNotFound        = Err("Basket not found")
+	ErrGetActiveBasketFailed = Err("Get non ordered basket failed")
+	ErrBasketItemsNotFound   = Err("Basket items not found")
 )
 
 type Err string
