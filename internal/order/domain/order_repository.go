b0c144f5085f283b46205f8ed0e88966e8433e9c
islom <ismoilovislom589@gmail.com>
2023-12-28 12:23:13 +0500

    small changing

--- internal/order/domain/order_repository.go
+++ internal/order/domain/order_repository.go
@@ -4,5 +4,5 @@ type OrderRepository interface {
 	CreateOrder(order Order) error
 	UpdateStatusOrder(orderID int, newStatus string) error
 	GetOrderByID(orderID int) (Order, error)
-	GetAllOrders() ([]Order, error)
+	GetPaginatedOrders(offset, limit int) ([]Order, error)
 }
