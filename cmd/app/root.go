fd12c835f6d74658a8df956e9fa6590e2ba3330a
samandar <samandar0311@gmail.com>
2024-02-01 12:30:36 +0500

    changes in grpc server

--- cmd/app/root.go
+++ cmd/app/root.go
@@ -2,72 +2,58 @@ package app
 
 import (
 	"fmt"
-	"golang-project-template/internal/common/postgres"
-	"net"
+	"log"
 	"os"
 
+	"golang-project-template/cmd/app/servers"
+
 	"golang-project-template/internal/common"
-	"golang-project-template/internal/pkg/jwt"
-	shopAdapters "golang-project-template/internal/shop/adapters"
-	service "golang-project-template/internal/shop/app"
-	"golang-project-template/internal/shop/domain"
-	shophandler "golang-project-template/internal/shop/ports/rest/handler"
-	userAdapters "golang-project-template/internal/users/adapters"
+	"golang-project-template/internal/pkg/config"
+	userRepo "golang-project-template/internal/users/adapters"
 	userApp "golang-project-template/internal/users/app"
-	"golang-project-template/internal/users/ports/grpc/proto/pb"
-	"golang-project-template/internal/users/ports/grpc/server"
-	userController "golang-project-template/internal/users/ports/http/controller"
-	"log"
-	"net/http"
+	userPb "golang-project-template/internal/users/ports/grpc/proto/pb"
+	userGrpcServer "golang-project-template/internal/users/ports/grpc/server"
 
 	"github.com/spf13/cobra"
 	"google.golang.org/grpc"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 )
 
 var rootCmd = &cobra.Command{Use: "run-grpc"}
 
+// command to run grpc server
 var userGrpcServerCmd = &cobra.Command{
 	Use: "user-grpc-server",
+
 	Run: func(cmd *cobra.Command, args []string) {
-		db, err := common.ConnectToDb(
-			os.Getenv("POSTGRES_HOST"),
+
+		db, err := common.ConnectToDb(os.Getenv("POSTGRES_HOST"),
 			os.Getenv("POSTGRES_PORT"),
 			os.Getenv("POSTGRES_DATABASE"),
 			os.Getenv("POSTGRES_USER"),
-			os.Getenv("POSTGRES_PASSWORD"),
-		)
+			os.Getenv("POSTGRES_PASSWORD"))
 
 		if err != nil {
-			log.Fatalf("Failed to connect to database: %v", err)
+			log.Fatalf("Failed to connect to database: %s", err)
 		}
-		userRepo := userAdapters.NewUserRepository(db)
+
+		config := config.NewConfig()
+		rpcPort := fmt.Sprintf(":%s", config.RpcPort)
+
+		userRepo := userRepo.NewUserRepository(db)
 		userUsecase := userApp.NewUserUsecase(userRepo)
-		userGrpcServer := server.NewUserGrpcServer(userUsecase)
 
-		lis, err := net.Listen("tcp", ":5006")
-		if err != nil {
-			log.Fatalf("failed to listen: %v", err)
-		}
-
-		fmt.Println("listening on port: 5006")
-
-		s := grpc.NewServer()
-
-		pb.RegisterUserServiceServer(s, userGrpcServer)
-		if err := s.Serve(lis); err != nil {
-			log.Fatalf("failed to serve: %v", err)
-		}
-
+		servers.RunGRPCServerOnAddr(rpcPort, func(server *grpc.Server) {
+			userSvc := userGrpcServer.NewUserGrpcServer(userUsecase)
+			userPb.RegisterUserServiceServer(server, userSvc)
+		})
 	},
 }
 
+// command to run http server
 var httpCmd = &cobra.Command{
 	Use: "http-server",
 	Run: func(cmd *cobra.Command, args []string) {
-		httpServer()
+		servers.RunHttpServer()
 	},
 }
 
@@ -79,78 +65,4 @@ func Execute() {
 		fmt.Fprintf(os.Stderr, "error while executing your CLI '%s'", err)
 		os.Exit(1)
 	}
-}
-
-func httpServer() *chi.Mux {
-	db, err := common.ConnectToDb(
-		os.Getenv("POSTGRES_HOST"),
-		os.Getenv("POSTGRES_PORT"),
-		os.Getenv("POSTGRES_DATABASE"),
-		os.Getenv("POSTGRES_USER"),
-		os.Getenv("POSTGRES_PASSWORD"),
-	)
-
-	if err != nil {
-		log.Fatalf("Failed to connect to database: %v", err)
-	}
-
-	dbNew, err := postgres.New(
-		os.Getenv("POSTGRES_HOST"),
-		os.Getenv("POSTGRES_PORT"),
-		os.Getenv("POSTGRES_DATABASE"),
-		os.Getenv("POSTGRES_USER"),
-		os.Getenv("POSTGRES_PASSWORD"),
-		"disable",
-	)
-
-	defer db.Close()
-
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
-
-	//Testing router
-	router.Get("/", func(w http.ResponseWriter, _ *http.Request) {
-		w.Write([]byte("Hello, Chi!"))
-	})
-
-	// User router
-	//userRepo := userAdapters.NewUserRepository(db)
-	userRepo := userAdapters.NewUserRepository(db)
-	userUsecase := userApp.NewUserUsecase(userRepo)
-	userHandler := userController.NewUserController(userUsecase)
-
-	// Shop router
-	shopRepo := shopAdapters.NewShopRepository(dbNew)
-	shopFactory := domain.NewShopFactory(256, 256)
-	shopService := service.NewShopService(shopRepo, shopFactory, userRepo)
-	shopHandler := shophandler.ShopHandler{ShopService: shopService}
-
-	// Routers
-	router.Route("/api", func(r chi.Router) {
-
-		r.Route("/users", func(r chi.Router) {
-			r.Post("/register-admin/", userHandler.RegisterAdminUserHandler)
-			r.Post("/register-merchant/", userHandler.RegisterMerchantHandler)
-			r.Post("/register-customer/", userHandler.RegisterCustomerHandler)
-			r.Post("/login/", userHandler.LoginUserHandler)
-			r.With(jwt.AuthMiddleWare).Get("/get-user/{phone_number}", userHandler.GetUserByPhoneNumberHandler)
-		})
-
-		r.Route("/shop", func(r chi.Router) {
-
-			r.Post("/", shopHandler.CreateShop)
-			r.Get("/{id}", shopHandler.GetShopById)
-			r.Get("/", shopHandler.GetAllShops)
-		})
-
-	})
-
-	server := &http.Server{Addr: os.Getenv("HTTP_PORT"), Handler: router}
-	log.Println("Starting server on port...", os.Getenv("HTTP_PORT"))
-	if err := server.ListenAndServe(); err != http.ErrServerClosed {
-		panic(err)
-	}
-	defer server.Close()
-
-	return router
 }
